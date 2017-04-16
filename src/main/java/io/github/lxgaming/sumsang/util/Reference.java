26358d582661d695d324dd400bb73f8022a81434
Alex Thomson <alexrobthomson@outlook.com>
2017-04-16 13:38:41 +1200

    Bump version

--- src/main/java/io/github/lxgaming/sumsang/util/Reference.java
+++ src/main/java/io/github/lxgaming/sumsang/util/Reference.java
@@ -20,9 +20,9 @@ public class Reference {
 	
 	public static final String MOD_ID = "sumsang";
 	public static final String MOD_NAME = "Sumsang";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.1.0";
 	public static final String ACCEPTED_VERSIONS = "[1.11.2]";
-	public static final String ACCEPTABLE_REMOTE_VERSIONS = "[1.0.0]";
+	public static final String ACCEPTABLE_REMOTE_VERSIONS = "[1.1.0]";
 	public static final String CERTIFICATE_FINGERPRINT = "565fa4dbf20e7c3c4423950ca8e0bdabf7568796";
 	
 	public static final String CLIENT_PROXY_CLASS = "io.github.lxgaming.sumsang.proxy.ClientProxy";
